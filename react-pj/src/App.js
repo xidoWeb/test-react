1249017053284a03ffc872f11d2e9e7c91ee15e4
xido <erivery2@gmail.com>
2022-01-18 17:12:26 +0900

    [add] react :ab: react-router 기능 처리

--- react-pj/src/App.js
+++ react-pj/src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import Header from './component/Header';
 import Footer from './component/Footer';
-import Home from './page/Home';
-import About from './page/About';
-import { Routes, Route } from 'react-router-dom';
+import RouterPage from './Router/RouterPage';
+import {BrowserRouter} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-
-      <Routes>
-        <Route element={<Home />} />
-        <Route element={<About />} />        
-      </Routes>
-
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <RouterPage />  
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
--- react-pj/src/component/Header.js
+++ react-pj/src/component/Header.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 function Header() {
   return (
-    <header id="headBox">header영역</header>
+    <header id="headBox">
+      <h1>header</h1>
+      <nav className='gnb'>
+        <NavLink to="/">home</NavLink> 
+          { ' | ' }    
+        <NavLink to="about">about</NavLink>
+      </nav>
+    </header>
   )
 }
 
